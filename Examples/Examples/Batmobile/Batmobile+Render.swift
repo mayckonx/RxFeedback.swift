94ee2e09fa558e7b9c1471c65d52bad8309fa2ee
Krunoslav Zaher <krunoslav.zaher@gmail.com>
2018-11-05 23:43:49 +0100

    Adding batmobile viewcontroller.

---
+++ Examples/Examples/Batmobile/Batmobile+Render.swift
@@ -0,0 +1,14 @@
+//
+//  Batmobile+Render.swift
+//  Example
+//
+//  Created by Krunoslav Zaher on 11/5/18.
+//  Copyright © 2018 Krunoslav Zaher. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public class BatmobileRootViewController: UIViewController {
+
+}
---
+++ Examples/Examples/Batmobile/Batmobile.swift
@@ -0,0 +1,9 @@
+//
+//  Batmobile.swift
+//  Example
+//
+//  Created by Krunoslav Zaher on 11/5/18.
+//  Copyright © 2018 Krunoslav Zaher. All rights reserved.
+//
+
+import Foundation
